78d1287fbc96d60fd7c4ad667ce0b39a8bed57db
suyingda <syq1023032120@qq.com>
2017-09-17 11:27:03 +0800

    增加组件通讯-未完

--- app/props/Myprops.js
+++ app/props/Myprops.js
@@ -1,35 +1,33 @@
 
 import React, { Component } from "react";
-import {PropTypes} from "prop-types";
+import { PropTypes } from "prop-types";
 class Myprops extends Component {
-  
-constructor(abc){
-    super();
-    console.log(abc)  //系统自动传入props
-    this.state={
-        a:abc.a,   
-    }
-}
 
- 
+    constructor(abc) {
+        super();
+        console.log(abc)  //系统自动传入props
+        this.state = {
+            a: abc.a,
+        }
+    }
     render() {
         return (
             <div>
                 {this.props.b}
-             <br/>
+                <br />
                 {this.state.a}
-                <br/>
-               
-                <br/>
-                <p onClick={()=>{{this.props.setD(10)}}}>点击改变父组件值</p>
+                <br />
+
+                <br />
+                <p onClick={() => { { this.props.setD(10) } }}>点击改变父组件值</p>
             </div>
         );
     }
 }
-Myprops.PropTypes={
-    a:PropTypes.number.isRequired,
-    b:PropTypes.string.isRequired,
-   /*  c:PropTypes.number.isRequired, */
- 
+Myprops.PropTypes = {
+    a: PropTypes.number.isRequired,
+    b: PropTypes.string.isRequired,
+    /*  c:PropTypes.number.isRequired, */
+
 }
 export default Myprops;
